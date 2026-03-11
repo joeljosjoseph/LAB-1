87697a3dfff8d0f50bfa5b08c9942d72c4011fc2
Joel <joeljosjoseph12@gmail.com>
2026-03-11 14:07:53 -0400

    Added discount feature by Joel

--- ProductManager.py
+++ ProductManager.py
@@ -1,2 +1,13 @@
 class ProductManager:
-    pass
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, name, price):
+        self.products.append({"name": name, "price": price})
+
+    def list_products(self):
+        return self.products
+
+    # This method calculates discount price for a product
+    def calculateDiscount(self, price, discount_percent):
+        return price - (price * discount_percent / 100)
